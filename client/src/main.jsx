69051e4430742358971c118d41582886a152aeb6
Alisha Jesani <alishajesani@Alishas-MacBook-Pro-5.local>
2026-02-20 23:31:26 -0800

    Deploy client to GitHub Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+document.documentElement.style.setProperty(
+  "--base-url",
+  import.meta.env.BASE_URL
 );
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
