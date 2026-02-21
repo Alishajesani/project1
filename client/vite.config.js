16fb4e1323c5f1d8676964075368c0c8c507ce0d
Alisha Jesani <alishajesani@Alishas-MacBook-Pro-5.local>
2026-02-20 23:27:54 -0800

    Deploy client to GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT for GitHub Pages project sites:
+  // https://alishajesani.github.io/project1/
   base: "/project1/",
+
+  // Build output goes to repo root /docs (so Pages can serve it)
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
 });
